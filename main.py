0db709214c4e590666750a97675f6d730f98e60a
Steven Weiss <weiss.steven@gmail.com>
2019-07-18 17:05:45 -0400

    added main function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+# from functions import *
 import time
+import os
 
 # def instructions():
 #     return
@@ -14,30 +16,40 @@ def add_new_name(name):
 def load(name):
     print('Starting program...')
     time.sleep(2) 
+    os.system('clear')
     print('\n')
     print('\t Welcome to NOMBRE')
     print('\t Hello', name)
     print('\n')
 
-def one():
+def name_adder():
     print("Who would you like to add?" )
     new_name = input(">>> ")
     add_new_name(new_name)
     print("\n")
     print("You have added", new_name)
     print("\n")
-    program_text()
+    menu_text()
 
-def three():
+def name_updater(a_name):
+    print("What memory would you like to associate with that person?" )
+    a_memory = input(">>> ")
+    update_name(a_name, a_memory)
+    print("\n")
+    print("You have updated,", a_name)
+    print("\n")
+    menu_text()
+
+def all_address_book():
     print( get_all_faker() )
-    program_text()
+    menu_text()
 
-def program_text():
+def menu_text():
     print('\t What would you care to do? Type: ')
     print('\n')
     print("\t 1: Load a new name")
     print("\t 2: Add to an old name")
-    print("\t 3: See you entire memory book")
+    print("\t 3: See your entire memory book")
     print("\t Keyword: To bring up a list of possible names" )
     print("\t Q: To quit" )
     print('\n')
@@ -46,32 +58,42 @@ def update_name(name, new_info):
     faker_db[name].append(new_info)
     return faker_db[name]
 
+def return_possibles(word):
+    term = input('Give me a word: ')
+    new_list = []
+
+    for key, value in fake.items():
+        if term in value:
+            new_list.append(key)
+
+    if len(new_list) == 0:
+        print("There is no match for this criteria")
+    else: 
+        print(new_list)
+
 def main(name):
     load(name)
-    program_text()
+    menu_text()
 
     while True:
         choice = input(">>> ")
 
         if choice == "1":
-            one()
+            name_adder()
 
         elif choice == "2":
             print("Who would you like to update?" )
             a_name = input(">>> ")
-            # check if name is in the db
+            
             # multiple entries
-
-            print("What memory would you like to associate with that person?" )
-            a_memory = input(">>> ")
-            update_name(a_name, a_memory)
-            print("\n")
-            print("You have updated,", a_name)
-            print("\n")
-            program_text()
+            if a_name in faker_db:
+                name_updater(a_name)
+            else:
+                print("That person does not exist.  Try again." )
+                menu_text()
 
         elif choice == "3":
-            three()
+            all_address_book()
 
         elif choice == "Q":
             print('Thank you for using Nombre.  See you later.')
@@ -79,19 +101,12 @@ def main(name):
             break
 
         else:
-            print("That is an invalid choice.  Try again or Hit 'Q' to exit")
-            print('\n')
-            program_text()
+            # this will be the memory recall 
+            return_possibles(word)
 
     # print("0: What is this app?")
 
-    # add_new_name('bruce')
-    # print('here is who is in your db', get_all_faker() )
-    # update_name('bruce', 'nyu')
-    # update_name('bruce', 'john')
-    # print(get_all_faker())
-
-main('steven')
+main('Mr Weiss')
 
 # def load_memory():
 #     """
